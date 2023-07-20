1a1249983f2fd4ceea88f2cc669f707a6467146e
Frasat ALi Gujjar <127954439+FrasatAliGujjar@users.noreply.github.com>
2023-07-20 12:48:56 -0700

    V1
    V1

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// ================================================================
+function whatsapp() {
+
+    let url = "http://wa.me/923419385624?text="
+
+        + "*Asslam-o-Alaikum !*" + " " + "%0a"
+        + " " + "%0a"
+
+
+    window.open(url, "_blank");
+
+}
+
